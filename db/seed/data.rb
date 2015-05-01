d32058d82d8fd110fdb4f616c9b03bee31b3e03b
hiro.t <hiro06061217@gmail.com>
2015-05-01 16:44:52 +0900

    ポケモンdataを挿入する際にmove_itemsも更新する

--- db/seed/data.rb
+++ db/seed/data.rb
@@ -5,24 +5,28 @@ require 'json'
 def translate_name_to_id(party_name)
   party_id = []
   party_name.each do |name|
+    if Pokemon.where(name:name).first == nil
+      puts "#{name} error___________" 
+      break
+    end
     poke_id = Pokemon.where(name:name).first["poke_id"]
     party_id.push(poke_id)
   end
   return party_id
 end
 
-
 def get_data
   partys = open(JSON_FILE_PATH) do |io|
     JSON.load(io)
   end
   partys.each do |party|
-    puts party
     simple_party = party["simple_party"]
+    puts simple_party
     detailed_party = party["detailed_party"]
     #id形式のパーティ情報
     party_id = translate_name_to_id(simple_party)
-    Datum.make_data(detailed_party,party_id)
+    new_data = Datum.make_data(detailed_party,party_id)
+    MoveItem.calculate_count_with(new_data)
   end
 end
 
